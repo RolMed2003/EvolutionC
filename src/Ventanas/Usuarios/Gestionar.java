e717214e148cd6a2341f0171a15c5eb80efff2a7
rolan <rolan@DESKTOP-069LLO3>
2022-04-05 15:09:01 -0600

    delete button user

--- src/Ventanas/Usuarios/Gestionar.java
+++ src/Ventanas/Usuarios/Gestionar.java
@@ -1,13 +1,18 @@
 package Ventanas.Usuarios;
 
+import Clases.Apoyo.Conexion;
 import Clases.Apoyo.PlaceHolder;
 import Clases.Generales.Usuario;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import java.sql.*;
 
 public class Gestionar extends javax.swing.JInternalFrame {
-    
+
     Usuario user = new Usuario();
-    
+
     public Gestionar() {
 
         initComponents();
@@ -16,22 +21,22 @@ public class Gestionar extends javax.swing.JInternalFrame {
         setTitle("Gestión de usuarios");
         setSize(790, 490);
         setLocation(245, 105);
-        
+
         //Placeholder.
         PlaceHolder X = new PlaceHolder("Nombre de usuario", buscarTxt);
-        
+
         //Tabla.
-        new Thread(){
-            
+        new Thread() {
+
             @Override
-            public void run(){
-                
+            public void run() {
+
                 loading.setVisible(true);
-                usuariosTbl.setModel(user.mostrarUsuarios((DefaultTableModel)usuariosTbl.getModel()));
+                usuariosTbl.setModel(user.mostrarUsuarios((DefaultTableModel) usuariosTbl.getModel()));
                 loading.setVisible(false);
-                
+
             }
-            
+
         }.start();
 
     }
@@ -44,8 +49,8 @@ public class Gestionar extends javax.swing.JInternalFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         usuariosTbl = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        editarBtn = new javax.swing.JButton();
+        deleteBtn = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         buscarTxt = new javax.swing.JTextField();
         buscarBtn = new javax.swing.JButton();
@@ -89,15 +94,25 @@ public class Gestionar extends javax.swing.JInternalFrame {
         jLabel1.setText("Tabla de usuarios");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 43, -1, -1));
 
-        jButton1.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Iconos/Login/editar-documento.png"))); // NOI18N
-        jButton1.setText("Editar");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(644, 132, 117, -1));
+        editarBtn.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
+        editarBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Iconos/Login/editar-documento.png"))); // NOI18N
+        editarBtn.setText("Editar");
+        editarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                editarBtnActionPerformed(evt);
+            }
+        });
+        getContentPane().add(editarBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(644, 132, 117, -1));
 
-        jButton2.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Iconos/Login/hombre.png"))); // NOI18N
-        jButton2.setText("Eliminar");
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(644, 191, -1, -1));
+        deleteBtn.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
+        deleteBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/Iconos/Login/hombre.png"))); // NOI18N
+        deleteBtn.setText("Eliminar");
+        deleteBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteBtnActionPerformed(evt);
+            }
+        });
+        getContentPane().add(deleteBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(644, 191, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
         jLabel2.setText("Buscar:");
@@ -118,19 +133,95 @@ public class Gestionar extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buscarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscarBtnActionPerformed
-        
+
         String buscar = buscarTxt.getText().trim();
-        
+
         usuariosTbl.setModel(user.mostrarUsuarios(buscar));
-        
+
     }//GEN-LAST:event_buscarBtnActionPerformed
+
+    private void editarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editarBtnActionPerformed
+
+
+    }//GEN-LAST:event_editarBtnActionPerformed
+
+    private void deleteBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteBtnActionPerformed
+
+        //Variables
+        int selectedRow = usuariosTbl.getSelectedRow();
+
+        //Validaciones
+        if (selectedRow == -1) {
+
+            Icon icon = new ImageIcon(getClass().getResource("../../Recursos/Iconos/JOption/advertencia.png"));
+            JOptionPane.showMessageDialog(null, "Por favor, seleccione un registro.", " -  Advertencia",
+                    JOptionPane.PLAIN_MESSAGE, icon);
+
+        } else {
+
+            int ID = (int) usuariosTbl.getValueAt(selectedRow, 0);
+
+            new Thread() {
+
+                @Override
+                public void run() {
+
+                    loading.setVisible(true);
+                    
+                    try {
+
+                        Connection cn = Conexion.conectar();
+                        PreparedStatement pst = cn.prepareStatement("delete from Usuarios where ID_USER = '" + ID + "'");
+
+                        pst.execute();
+
+                        Icon icon = new ImageIcon(getClass().getResource("../../Recursos/Iconos/JOption/cheque.png"));
+                        JOptionPane.showMessageDialog(null, "Registro eliminado exitosamente.", " -  Info", JOptionPane.PLAIN_MESSAGE,
+                                icon);
+
+                        new Thread() {
+
+                            @Override
+                            public void run() {
+
+                                loading.setVisible(true);
+
+                                DefaultTableModel model = (DefaultTableModel) usuariosTbl.getModel();
+                                while (model.getRowCount() != 0) {
+
+                                    model.removeRow(0);
+
+                                }
+                                usuariosTbl.setModel(user.mostrarUsuarios(model));
+
+                                loading.setVisible(false);
+
+                            }
+
+                        }.start();
+
+                    } catch (SQLException e) {
+
+                        System.err.println("Error al eliminar usuario" + e);
+
+                    }
+
+                    loading.setVisible(false);
+                    
+                }
+
+            }.start();
+
+        }
+
+    }//GEN-LAST:event_deleteBtnActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buscarBtn;
     private javax.swing.JTextField buscarTxt;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton deleteBtn;
+    private javax.swing.JButton editarBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
